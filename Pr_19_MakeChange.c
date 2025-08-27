ede4209a4247ef4a17f572ff8d60ac74e337d23e
Avinash-Savaliya-5 <absavaliya2005@gmail.com>
2025-08-27 13:35:05 +0530

    Design & Analysis of Algorithm

--- Pr_19_MakeChange.c
+++ Pr_19_MakeChange.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-#include<limits.h>
-#define MAX 100
+#include <stdio.h>
+#include <limits.h>
+
 int minCoins(int coins[], int m, int V) {
     int dp[V + 1];
     for (int i = 0; i <= V; i++) dp[i] = INT_MAX;
     dp[0] = 0;
+
     for (int i = 1; i <= V; i++) {
         for (int j = 0; j < m; j++) {
             if (coins[j] <= i) {
@@ -18,14 +19,24 @@ int minCoins(int coins[], int m, int V) {
 }
 
 int main() {
-    int coins[] = {1, 3, 4, 5};
-    int m = sizeof(coins) / sizeof(coins[0]);
+    int n, V;
+    printf("Enter the number of coin denominations: ");
+    scanf("%d", &n);
+
+    int coins[n];
+    for (int i = 0; i < n; i++) {
+        printf("Enter denomination %d: ", i + 1);
+        scanf("%d", &coins[i]);
+    }
+
     printf("Enter the value to make change for: ");
-    int V = scanf("%d", &V);
-    int result = minCoins(coins, m, V);
+    scanf("%d", &V);
+
+    int result = minCoins(coins, n, V);
     if (result != -1)
         printf("Minimum coins required is %d\n", result);
     else
         printf("Not possible to make change for %d with given coins\n", V);
+
     return 0;
 }
